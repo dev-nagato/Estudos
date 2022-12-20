b4c03d5f5c567ea1bfce4ef7b0360b8dcec8191d
Pedro Martins <103450015+dev-nagato@users.noreply.github.com>
2022-12-20 14:12:54 -0500

    adding object review

---
+++ RevisaoDeObjetos.js
@@ -0,0 +1,16 @@
+// objeto
+
+const person = {
+    nome:'nagato',
+    idade:19,
+    peso:85,
+    administrador:true
+}
+
+// pegando propriedades no objeto
+
+//console.log(person.name)
+
+// pegando dados no objeto com interpolação
+
+console.log(`${person.nome} tem ${person.idade} anos`)
