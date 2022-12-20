f08dc7e85deeed176d5555eb141adf024e0837de
Pedro Martins <103450015+dev-nagato@users.noreply.github.com>
2022-12-20 14:06:05 -0500

    adding the operetors

---
+++ ExpressoesEOperadores.js
@@ -0,0 +1,18 @@
+/* Expressões e Operadores
+
+binary
+unary
+ternary
+*/
+
+let number = 1
+
+console.log(number + 1) // operador binary
+
+let number_2 = 3
+
+console.log(++number_2) // operador unary
+
+let number_3 = 5
+
+console.log(true ? 'alo' : 'nada') // operador ternary , se for true imprime alo , se for false imprime nada
