4563c343daa535abb4232d26bf9c2cac47dafc67
Pedro Martins <103450015+dev-nagato@users.noreply.github.com>
2022-12-20 14:02:35 -0500

    delving into objects

---
+++ Exercicios.js
@@ -0,0 +1,79 @@
+
+let peso = 35
+console.log(typeof peso)
+
+let aluno = {
+    name: 'nagato',
+    age: 19,
+    weigth: 85,
+    isSubscribed: true
+}
+console.log(`${aluno.name} de idade ${aluno.age} pesa ${aluno.stars}`)
+
+
+let student = [
+    {
+        name: 'nagato',
+        age: '19',
+        weigth:'85',
+        isSubscribed: true
+    },
+    rupau
+    
+]
+
+const rupau = {
+    name: rupau,
+    age:21,
+    weigth:70,
+    isSubscribed:true
+}
+
+console.log(student[0])
+
+// new list
+
+const rupauzin = {
+    name: 'rupau',
+    age: 21,
+    weigth: 70,
+    isSubscribed: true
+
+}
+const nagatinho = {
+    name: 'nagatinho',
+    age: 19,
+    weigth : 85,
+    isSubscribed: true
+}
+
+const alunos = [
+    rupauzin,
+    nagatinho
+]
+
+const NumeroNaChamada = {
+    number1: 'pedro',
+    number2: 'marcos',
+    number3: 'wesley',
+    number4: 'gustavo',
+    number5: 'marlon',
+    number6: 'guilherme'
+    
+}
+
+console.log(`o aluno ${NumeroNaChamada.number1} esta de recuperação`)
+
+const totalDealunos = [
+    'nagato',
+    1,
+    true,
+
+    {
+      nome: 'Daniel',
+      numeroNaFicha: 5,
+      aprovado: true
+    }
+]
+
+console.log(` ${totalDealunos[3].nome} esta aprovado`)
