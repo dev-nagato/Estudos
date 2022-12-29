6ec25df4dc1b195cdc7a3ba8f68d44ce33a45ca5
Pedro Martins <103450015+dev-nagato@users.noreply.github.com>
2022-12-29 12:53:20 -0500

    adding new function

---
+++ new.js
@@ -0,0 +1,12 @@
+/*
+new
+    *criar um novo objeto
+*/
+
+let nome = new String('Pedro')
+nome.SobreNome = 'Martins'
+let idade = new Number(19)
+console.log(nome.SobreNome, idade)
+
+let date = new Date('22-20-12')
+console.log(date)
