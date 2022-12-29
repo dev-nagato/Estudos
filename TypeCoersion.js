5743ed455e761e37016cf1024f4ac2dc0c11ca6b
Pedro Martins <103450015+dev-nagato@users.noreply.github.com>
2022-12-29 13:12:35 -0500

    adding type information

---
+++ TypeCoersion.js
@@ -0,0 +1,6 @@
+/* type conversion vs  type coersion
+
+alteração de um tipo de dado para o outro */
+
+console.log('9' + 5) // desa forma é uma coersion onde o codigo sera alterado para string automaticamente
+console.log(Number('9') + 5) // essa forma é conversion, pois eu estou alterando manualmente a string
