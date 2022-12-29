fe640153817a50b460c5debcdea7505cfb72eb06
Pedro Martins <103450015+dev-nagato@users.noreply.github.com>
2022-12-29 13:10:05 -0500

    adding more operator information

---
+++ OperatorPrescedence.js
@@ -0,0 +1,14 @@
+/* Operator precedence (Precedencia de Operadores)
+
+* grouping                          ( )
+* negação e incremento              ! ++ --
+* multiplicação e divisão           * /
+* adição e subtração                + -
+* relacional                        < <= > >=c
+* igualdade                         == != === !==
+* AND                               &&
+* OR                                ||
+* condicional                       ?:
+* assigment (atribuição)            = += -= *= %=
+*/
+console.log( (2+5) * 10) // grouping ()
