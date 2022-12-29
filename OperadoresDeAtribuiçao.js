d27f625f1135e01d7c8b5cfd0e098556133bd127
Pedro Martins <103450015+dev-nagato@users.noreply.github.com>
2022-12-29 13:06:19 -0500

    add a new operator

---
+++ OperadoresDeAtribuiçao.js
@@ -0,0 +1,26 @@
+//operadores de atribuição (Assigment)
+
+let x
+console.log(x)
+
+// assigment
+// x = 1
+
+// adiction     assigment
+// x = x + 2
+// x += 2
+
+// subtraction assigment
+// x = x - 1
+// x -= 1
+
+// multiplication assigment
+// x = x * 2
+// x *= 2
+
+// division assigment
+// x = x / 2
+// x /= 2
+
+// remainder, exponetiation
+// x **= 2
